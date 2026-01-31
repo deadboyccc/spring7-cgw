279af07cea3a5666d7fd91aec64f54730917beab
Ahmed M. Ali <ahmedmohammed199975@gmail.com>
2026-01-31 07:02:33 +0300

    add initial application configuration and update dependencies

---
+++ src/main/java/dev/dead/spring7cgw/GatewayConfig.java
@@ -0,0 +1,25 @@
+package dev.dead.spring7cgw;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.stripPrefix;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouterFunction<ServerResponse> gatewayRoutes() {
+        return route("dummy_routes")
+                .route(path("/json/**"), http("https://jsonplaceholder.typicode.com")).filter(setPath("/posts"))
+                .route(path("/httpbin/**"), http("https://httpbin.org")).filter(stripPrefix(1))
+                .route(path("/dummy/**"), http("https://dummyjson.com")).filter(stripPrefix(1))
+                .build();
+    }
+}
