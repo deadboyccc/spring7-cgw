99905b028a63cc1a0ae4f020646bcb07fe0bbf88
Ahmed M. Ali <ahmedmohammed199975@gmail.com>
2026-01-31 05:40:48 +0300

    init

---
+++ src/main/java/dev/dead/spring7cgw/Spring7CgwApplication.java
@@ -0,0 +1,13 @@
+package dev.dead.spring7cgw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7CgwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring7CgwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/dead/spring7cgw/Spring7CgwApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dead.spring7cgw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7CgwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
